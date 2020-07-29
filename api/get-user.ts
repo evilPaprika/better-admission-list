7bb6adf3545f8fe64928601d23c0a6f017bd555d
evilpaprika <ivil.paprika@gmail.com>
2020-07-29 23:08:57 +0500

    Revert "test serverless api"
    This reverts commit dd6b3791601a04f223e0ff331bdae40d80d4487d.

--- api/get-user.ts
+++
@@ -1,7 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-
-
-export default (req: NowRequest, res: NowResponse) => {
-    res.status(200);
-    res.json({ name: 'John', email: 'john@example.com' });
-};
